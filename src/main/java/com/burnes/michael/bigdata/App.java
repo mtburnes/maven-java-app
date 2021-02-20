e1983cfa74ea0dd079ac085770fcde46cad34d3d
Michael Burnes <me@mtburnes.dev>
2021-02-19 18:33:30 -0600

    First Commit

---
+++ src/main/java/com/burnes/michael/bigdata/App.java
@@ -0,0 +1,13 @@
+package com.burnes.michael.bigdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
